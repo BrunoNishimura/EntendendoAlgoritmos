5f32fdd1b3ce4cdef8f639b34c7bbf528b608337
Bruno Y. Nishimura <72320100+BrunoNishimura@users.noreply.github.com>
2023-12-03 23:34:55 -0300

    Cap1: Pesquisa Binária.

---
+++ EntendendoAlgoritmos/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+using EntendendoAlgoritmos.Cap1;
+
+namespace CursoCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var central = new CentralDeExercicios(new Dictionary<string, Action>() {
+                //Capítulo 1
+                {"Pesquisa Binária | Binary Search", PesquisaBinaria.Executar},
+                
+            });
+
+            central.SelecionarEExecutar();
+        }
+    }
+}
