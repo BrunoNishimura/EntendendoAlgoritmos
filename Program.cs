eef5c7a2d1ea7183f15e119e9c1737c2665c892d
BrunoNishimura <by.nishimura@gmail.com>
2024-01-03 23:43:12 -0300

    Cap2: Selection Sort

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 
 using EntendendoAlgoritmos.Cap1;
+using EntendendoAlgoritmos.Cap2;
 
 namespace CursoCSharp
 {
@@ -12,7 +13,10 @@ namespace CursoCSharp
             var central = new CentralDeExercicios(new Dictionary<string, Action>() {
                 //Capítulo 1 - Introduction to algorithms
                 {"Pesquisa Binária | Binary Search", PesquisaBinaria.Executar},
-                
+                                
+                //Capítulo 2 - Selection sort
+                {"Selection sort", Cap2_SelectionSort.Executar},
+
             });
 
             central.SelecionarEExecutar();
