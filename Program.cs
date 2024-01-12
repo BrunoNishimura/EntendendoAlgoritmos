b102325afe3f42d0010ee29c0a3ce3a64e226ab1
BrunoNishimura <by.nishimura@gmail.com>
2024-01-11 23:56:38 -0300

    Cap4: Recursive Max

--- Program.cs
+++ Program.cs
@@ -28,7 +28,8 @@ namespace CursoCSharp
                 //Capítulo 4 - Quicksort
                 {"Loop Sum", Cap4_01_Loop_Sum.Executar},
                 {"Recursive Sum", Cap4_02_Recursive_Sum.Executar},
-                {"Recursive Count", Cap4_03_Recursive_Count.Executar}
+                {"Recursive Count", Cap4_03_Recursive_Count.Executar},
+                {"Recursive Max", Cap4_03_Recursive_Count.Executar},
 
             })
             {
