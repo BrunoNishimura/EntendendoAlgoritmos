189aff919aca0ab5cb10f0b19c16eed20befea99
BrunoNishimura <by.nishimura@gmail.com>
2024-08-25 21:23:53 -0300

    fix: CPNU-RevertSequence

--- Codewars/kyu_6/CPNU-RevertSequence.cs
+++ Codewars/kyu_6/CPNU-RevertSequence.cs
@@ -9,25 +9,29 @@ namespace EntendendoAlgoritmos.Codewars.kyu_6
     internal class CPNU_RevertSequence
     {
 
-public int InverterDigitos(int numero)
-{
-    int resultado = 0;
-    
-    while (numero > 0)
-    {
-        int digito = numero % 10;       // Obtém o último dígito
-        resultado = resultado * 10 + digito;  // Adiciona o dígito ao resultado
-        numero /= 10;                   // Remove o último dígito do número
-    }
-    
-    return resultado;
-}
+        public static int InverterDigitos(int numero)
+        {
+            int resultado = 0;
+
+            while (numero > 0)
+            {
+                int digito = numero % 10; // Obtém o último dígito
+                resultado = resultado * 10 + digito; // Adiciona o dígito ao resultado
+                numero /= 10; // Remove o último dígito do número
+            }
+
+            return resultado;
+        }
 
         public static void Executar()
         {
-            int teste = 1234;
+            int teste1 = 1234;
+            int teste2 = 1000;
+            int teste3 = 4;
 
-            Console.WriteLine(InverterDigitos(teste));
+            Console.WriteLine(InverterDigitos(teste1));
+            Console.WriteLine(InverterDigitos(teste2));
+            Console.WriteLine(InverterDigitos(teste3));
         }
     }
 }
